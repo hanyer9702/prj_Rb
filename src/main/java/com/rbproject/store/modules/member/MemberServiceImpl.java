a5001442dccd15ea9e3fcca19aa376f8841ca861
owner <hanyer9702@naver.com>
2022-03-10 20:12:52 +0900

    220310

---
+++ src/main/java/com/rbproject/store/modules/member/MemberServiceImpl.java
@@ -0,0 +1,23 @@
+package com.rbproject.store.modules.member;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+	@Autowired
+	MemberDao dao;
+	
+	@Override
+	public List<Member> selectList() throws Exception {
+		return dao.selectList(); 
+	}
+	
+	@Override
+	public int insert(Member dto) throws Exception {
+		return dao.insert(dto); 
+	}
+}
